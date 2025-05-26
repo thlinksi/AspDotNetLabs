7bc7759c751819338ddd0728acf3f29feca2f9fe
Lina Bubon <ipz235_blv@student.ztu.edu.ua>
2025-05-26 19:46:59 +0300

    Lab5

---
+++ Lab5/Lab5 (2)/SharedLibrary/Repositories/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using SharedLibrary.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetAllAsync();
+        Task<User> GetByIdAsync(int id);
+        Task<User> GetByUsernameAsync(string username);
+        Task<User> CreateAsync(User user);
+        Task<User> UpdateAsync(User user);
+        Task<bool> DeleteAsync(int id);
+    }
+}
---
+++ Lab5/Lab5 (2)/SharedLibrary/Repositories/UserRepository.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SharedLibrary.Data;
+using SharedLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext _context;
+
+        public UserRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<User>> GetAllAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<User> GetByIdAsync(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task<User> GetByUsernameAsync(string username)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Username == username);
+        }
+
+        public async Task<User> CreateAsync(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task<User> UpdateAsync(User user)
+        {
+            _context.Users.Update(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var user = await GetByIdAsync(id);
+            if (user == null) return false;
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
