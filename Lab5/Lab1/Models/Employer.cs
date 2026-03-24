1daeb23be37a898d21e63704fe4c522ae65b2dbd
Lina Bubon <ipz235_blv@student.ztu.edu.ua>
2026-03-24 13:40:01 +0200

    Lab6

--- Lab5/Lab1/Models/Employer.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Lab5.Models
-{
-    public class Employer
-    {
-        public string EmployerId { get; set; } 
-        public string CompanyName { get; set; } = String.Empty;
-        public ICollection<Vacancy> Vacancies { get; set; } = new List<Vacancy>();
-    }
-}
--- Lab5/Lab1/Models/ViewModels/ProfileViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Lab5.Models.ViewModels
-{
-    public class ProfileViewModel
-    {
-        public string Email { get; set; }
-        public string Role { get; set; }
-    }
-}
